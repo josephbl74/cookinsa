07a50b55814f5e0786d1c23ba9f251fff81f664c
josephbl74 <79768976+josephbl74@users.noreply.github.com>
2022-05-11 23:29:55 +0200

    2nd one

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <recipe_feed />
+    <recipecard
+      v-for="cardInfo in recipeCardsInfo"
+      :key="cardInfo.id"
+      :recipeSection="cardInfo"
+    />
+
+    <recipe-details
+      v-for="cardInfo in recipeCardsInfo"
+      :key="cardInfo.id"
+      :recipeSection="cardInfo"
+    />
+    
+  </div>
+</template>
+
+<script>
+  import recipecard from '../components/recipecard.vue'
+  import recipe_feed from '../components/recipe_feed.vue'
+  import recipe_details from '../components/recipe_details.vue'
+
+  import { recipe_cards_data } from "@/assets/data.js"
+
+  export default {
+    components: { recipe_feed, recipecard, recipe_details },
+    name: 'IndexPage',
+    data() {
+      return {
+        recipeCardsInfo: recipe_cards_data
+      }
+    }
+  }
+</script>
+
+<style>
+  .container {
+    margin-bottom: 5rem;
+  }
+</style>
